b6988115f41b0e50ce3937c9dce5a2602ac0be97
Adarsh Chandran <adarsh.chndrn@gmail.com>
2021-07-09 13:41:07 +0530

    Add search.js and example.html

--- search.js
+++ search.js
@@ -1,8 +1,8 @@
 const script = document.createElement("script")
 script.src = "https://code.jquery.com/jquery-3.4.1.min.js"
 script.type = "text/javascript"
-script.onreadystatechange = handler
-script.onload = handler
+// script.onreadystatechange = handler
+// script.onload = handler
 document.getElementsByTagName("head")[0].appendChild(script)
 
 // $("head").append(
@@ -20,7 +20,7 @@ var historyArr = []
 
 // var settings = {}
 
-function handler(color = "black") {
+function storybrainSearch(color = "black") {
   console.log("colll", color)
   $.ajaxSetup({
     async: false
